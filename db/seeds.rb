0af57ae41721d270d4a028dc8e304b3d27c9dab3
Samuel Lauricella <samuellauricella@Samuels-MacBook-Pro.local>
2017-09-21 12:57:44 -0400

    created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |blog|
+	Blog.create!(
+		title: "My blog post #{blog}",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt iaculis mauris, commodo volutpat nisi. Nulla ornare ornare nisl malesuada mattis. In porttitor metus eu turpis pulvinar, laoreet dignissim libero posuere. Nulla pretium elit in arcu viverra vehicula. Etiam fermentum justo sit amet magna tristique pretium. Phasellus ultricies, velit at pretium ultricies, enim magna condimentum dui, sit amet commodo sapien lacus sed enim. Maecenas nec ligula pulvinar, maximus nisi at, consequat turpis. Cras luctus arcu at mollis condimentum. Praesent dignissim in ipsum a gravida. Etiam rhoncus odio eget diam facilisis sagittis. Phasellus placerat, tortor pellentesque tempor volutpat, lectus diam accumsan nulla, vel vulputate ipsum arcu id libero. Vivamus vitae libero nec nisl aliquam iaculis. Cras vulputate ligula eu tortor dignissim venenatis. Pellentesque venenatis nisi vel vestibulum cursus. Pellentesque viverra molestie quam."
+		)
+
+end
+
+
+puts "10 Blog puts created"
+
+5.times do |skill|
+	Skill.create!(
+		title: " Rails #{skill}",
+		percent_utilized: 15
+		)
+end
+
+puts "5 skills created"
+
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title: #{portfolio_item}",
+		subtitle: "My Great Service",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt iaculis mauris, commodo volutpat nisi. Nulla ornare ornare nisl malesuada mattis. In porttitor metus eu turpis pulvinar, laoreet dignissim libero posuere. Nulla pretium elit in arcu viverra vehicula. Etiam fermentum justo sit amet magna tristique pretium. Phasellus ultricies, velit at pretium ultricies, enim magna condimentum dui, sit amet commodo sapien lacus sed enim. Maecenas nec ligula pulvinar, maximus nisi at, consequat turpis. Cras luctus arcu at mollis condimentum. Praesent dignissim in ipsum a gravida. Etiam rhoncus odio eget diam facilisis sagittis. Phasellus placerat, tortor pellentesque tempor volutpat, lectus diam accumsan nulla, vel vulputate ipsum arcu id libero. Vivamus vitae libero nec nisl aliquam iaculis. Cras vulputate ligula eu tortor dignissim venenatis. Pellentesque venenatis nisi vel vestibulum cursus. Pellentesque viverra molestie quam.",
+		main_image: "http://via.placeholder.com/600x200" ,
+		thumb_image:"http://via.placeholder.com/350x200"
+		)
+end
+puts "9 Portfolio items created"
