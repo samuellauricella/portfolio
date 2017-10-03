3011776679ea264f894d9f16ece4f2e18e914dd4
Samuel Lauricella <samuellauricella@Samuels-MacBook-Pro.local>
2017-10-03 18:53:49 -0400

    impelemented custom scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+3.times do |topic|
+	Topic.create!(title: "Topic #{topic}")
+end
+
 10.times do |blog|
 	Blog.create!(
 		title: "My blog post #{blog}",
-		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt iaculis mauris, commodo volutpat nisi. Nulla ornare ornare nisl malesuada mattis. In porttitor metus eu turpis pulvinar, laoreet dignissim libero posuere. Nulla pretium elit in arcu viverra vehicula. Etiam fermentum justo sit amet magna tristique pretium. Phasellus ultricies, velit at pretium ultricies, enim magna condimentum dui, sit amet commodo sapien lacus sed enim. Maecenas nec ligula pulvinar, maximus nisi at, consequat turpis. Cras luctus arcu at mollis condimentum. Praesent dignissim in ipsum a gravida. Etiam rhoncus odio eget diam facilisis sagittis. Phasellus placerat, tortor pellentesque tempor volutpat, lectus diam accumsan nulla, vel vulputate ipsum arcu id libero. Vivamus vitae libero nec nisl aliquam iaculis. Cras vulputate ligula eu tortor dignissim venenatis. Pellentesque venenatis nisi vel vestibulum cursus. Pellentesque viverra molestie quam."
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt iaculis mauris, commodo volutpat nisi. Nulla ornare ornare nisl malesuada mattis. In porttitor metus eu turpis pulvinar, laoreet dignissim libero posuere. Nulla pretium elit in arcu viverra vehicula. Etiam fermentum justo sit amet magna tristique pretium. Phasellus ultricies, velit at pretium ultricies, enim magna condimentum dui, sit amet commodo sapien lacus sed enim. Maecenas nec ligula pulvinar, maximus nisi at, consequat turpis. Cras luctus arcu at mollis condimentum. Praesent dignissim in ipsum a gravida. Etiam rhoncus odio eget diam facilisis sagittis. Phasellus placerat, tortor pellentesque tempor volutpat, lectus diam accumsan nulla, vel vulputate ipsum arcu id libero. Vivamus vitae libero nec nisl aliquam iaculis. Cras vulputate ligula eu tortor dignissim venenatis. Pellentesque venenatis nisi vel vestibulum cursus. Pellentesque viverra molestie quam.",
+		topic_id: Topic.last.id
 		)
 
 end
@@ -28,10 +33,20 @@ end
 puts "5 skills created"
 
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
 	Portfolio.create!(
 		title: "Portfolio title: #{portfolio_item}",
-		subtitle: "My Great Service",
+		subtitle: "Ruby on Rails",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt iaculis mauris, commodo volutpat nisi. Nulla ornare ornare nisl malesuada mattis. In porttitor metus eu turpis pulvinar, laoreet dignissim libero posuere. Nulla pretium elit in arcu viverra vehicula. Etiam fermentum justo sit amet magna tristique pretium. Phasellus ultricies, velit at pretium ultricies, enim magna condimentum dui, sit amet commodo sapien lacus sed enim. Maecenas nec ligula pulvinar, maximus nisi at, consequat turpis. Cras luctus arcu at mollis condimentum. Praesent dignissim in ipsum a gravida. Etiam rhoncus odio eget diam facilisis sagittis. Phasellus placerat, tortor pellentesque tempor volutpat, lectus diam accumsan nulla, vel vulputate ipsum arcu id libero. Vivamus vitae libero nec nisl aliquam iaculis. Cras vulputate ligula eu tortor dignissim venenatis. Pellentesque venenatis nisi vel vestibulum cursus. Pellentesque viverra molestie quam.",
+		main_image: "http://via.placeholder.com/600x200" ,
+		thumb_image:"http://via.placeholder.com/350x200"
+		)
+end
+
+1.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title: #{portfolio_item}",
+		subtitle: "Angular",
 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt iaculis mauris, commodo volutpat nisi. Nulla ornare ornare nisl malesuada mattis. In porttitor metus eu turpis pulvinar, laoreet dignissim libero posuere. Nulla pretium elit in arcu viverra vehicula. Etiam fermentum justo sit amet magna tristique pretium. Phasellus ultricies, velit at pretium ultricies, enim magna condimentum dui, sit amet commodo sapien lacus sed enim. Maecenas nec ligula pulvinar, maximus nisi at, consequat turpis. Cras luctus arcu at mollis condimentum. Praesent dignissim in ipsum a gravida. Etiam rhoncus odio eget diam facilisis sagittis. Phasellus placerat, tortor pellentesque tempor volutpat, lectus diam accumsan nulla, vel vulputate ipsum arcu id libero. Vivamus vitae libero nec nisl aliquam iaculis. Cras vulputate ligula eu tortor dignissim venenatis. Pellentesque venenatis nisi vel vestibulum cursus. Pellentesque viverra molestie quam.",
 		main_image: "http://via.placeholder.com/600x200" ,
 		thumb_image:"http://via.placeholder.com/350x200"
