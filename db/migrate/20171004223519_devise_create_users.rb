53e74953de5f6d3627f350afa968427ce3254560
Samuel Lauricella <samuellauricella@Samuels-MacBook-Pro.local>
2017-10-06 12:23:31 -0400

    added custom attributes to devise

--- db/migrate/20171004223519_devise_create_users.rb
+++ db/migrate/20171004223519_devise_create_users.rb
@@ -6,7 +6,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.string :encrypted_password, null: false, default: ""
 
       ##custom fields
-
       t.string :name
       ## Recoverable
       t.string   :reset_password_token
